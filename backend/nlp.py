57b91782794c31910d05c46fee87bbe797db12b6
Fennec2000GH <qcaijun2013@gmail.com>
2021-07-25 01:20:14 -0400

    Google Cloud NLP working

--- backend/nlp.py
+++ backend/nlp.py
@@ -2,12 +2,18 @@
 import os
 from dotenv import load_dotenv
 from google.cloud import language_v1
+from google.oauth2 import service_account
 from pprint import pprint
 
 
 # Instantiates a client
 load_dotenv()
-credentials = os.getenv(key='credentials')
+KEYDIR_PATH = os.getenv('KEYDIR_PATH')
+if __debug__:
+    pprint('-' * 100)
+    pprint(f'KEYDIR_PATH: {KEYDIR_PATH}')
+
+credentials = service_account.Credentials.from_service_account_file(filename=KEYDIR_PATH)
 client = language_v1.LanguageServiceClient(credentials=credentials)
 
 text = u"Hello, world!"
@@ -16,12 +22,13 @@ document = language_v1.Document(content=text, type_=language_v1.Document.Type.PL
 # Detects the sentiment of the text
 sentiment = client.analyze_sentiment(request={'document': document}).document_sentiment
 
-pprint('-' * 100)
-pprint('Text:')
-pprint('-' * 100)
-pprint(f'{text}')
-pprint('-' * 100)
-pprint('Sentiment:')
-pprint('-' * 100)
-pprint(f'Score: {sentiment.score}')
-pprint(f'Magnitude: {sentiment.magnitude}')
+if __debug__:
+    pprint('-' * 100)
+    pprint('Text:')
+    pprint('-' * 100)
+    pprint(f'{text}')
+    pprint('-' * 100)
+    pprint('Sentiment:')
+    pprint('-' * 100)
+    pprint(f'Score: {sentiment.score}')
+    pprint(f'Magnitude: {sentiment.magnitude}')
