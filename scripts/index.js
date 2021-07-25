aabf895cd0984266f994d4bf1e57aaf62ec69155
Fennec2000GH <qcaijun2013@gmail.com>
2021-07-25 09:35:57 -0400

    improved chatbot

---
+++ scripts/index.js
@@ -0,0 +1,28 @@
+function chat() {
+    // var chatMail = document.querySelector("#");
+    // var chat = chatMail.children;
+    var messageInput = document.querySelector("#message-input");
+    var messageBar = document.querySelector("#message-bar");
+    var chat = messageInput.value;
+    messageBar.insertAdjacentHTML("beforebegin",
+        ` 
+        <br/>
+        <div class="col-md-12 ">
+            <div class="col-md-12 text-center mb-2">
+                <p> ${chat} </p>
+            </div>
+        </div>
+        `
+    );
+
+    messageBar.insertAdjacentHTML("beforebegin",
+        ` 
+        <div class="row">
+            <div class="col-md-12 text-center mb-2">
+                <p> Yes. </p>
+            </div>
+        </div>
+        `
+    );
+    console.log(chat);
+}
